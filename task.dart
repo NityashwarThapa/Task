253f1c48328ba8bb0579202221348336240fff3d
NityashwarThapa <136233600+NityashwarThapa@users.noreply.github.com>
2024-11-19 08:30:54 +0545

    task1

---
+++ task.dart
@@ -0,0 +1,16 @@
+// This is a simple Dart program
+
+void main() {
+  // Print a greeting
+  print('Hello, World!');
+
+  // Declare two numbers
+  int num1 = 10;
+  int num2 = 20;
+
+  // Calculate their sum
+  int sum = num1 + num2;
+
+  // Print the result
+  print('The sum of $num1 and $num2 is $sum');
+}
